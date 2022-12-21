099448861104df8515d29997582f0861c908849e
FelipeWeiss <felipeew19@gmail.com>
2022-12-21 16:51:42 -0300

    feat(transactions): Created transactions context

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import { Transactions } from "./Transactions";
 
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Transactions />
+      
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
