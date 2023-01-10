e8d7de3873a539a314271eaf8e9a25d31d5783f5
FelipeWeiss <felipeew19@gmail.com>
2023-01-09 22:52:22 -0300

    improve(performance): applied react memo in SearchForm

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react'
 import { MagnifyingGlass } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { useContextSelector } from 'use-context-selector'
@@ -14,7 +15,7 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export const SearchForm = () => {
+const SearchFormComponent = () => {
   const fetchTransactions = useContextSelector(
     TransactionsContext,
     (context) => {
@@ -49,3 +50,5 @@ export const SearchForm = () => {
     </SearchFormContainer>
   )
 }
+
+export const SearchForm = memo(SearchFormComponent)
